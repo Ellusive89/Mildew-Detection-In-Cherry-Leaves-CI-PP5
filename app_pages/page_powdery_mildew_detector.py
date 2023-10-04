5d579014e0e3a8a63c0f08aa4f7b130101f3e765
Ellusive89 <>
2023-10-04 17:03:32 +0000

    Fixing issue with ANTIALIAS from Pillow library by changing it to LANCZOS

--- app_pages/page_powdery_mildew_detector.py
+++ app_pages/page_powdery_mildew_detector.py
@@ -7,8 +7,7 @@ from src.data_management import download_dataframe_as_csv
 from src.machine_learning.predictive_analysis import (
     load_model_and_predict,
     resize_input_image,
-    plot_predictions_probabilities
-)
+    plot_predictions_probabilities)
 
 
 def page_powdery_mildew_detector_body():
