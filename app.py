c94c383aa3c38da89ea895db7d931136f3825785
Ellusive89 <>
2023-10-05 15:36:25 +0000

    Add ML performance page

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from app_pages.multipage import MultiPage
 
 from app_pages.page_cherry_leaves_visualizer import page_cherry_leaves_visualizer_body
 from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
 
 app = MultiPage(app_name="Mildew Detection In Cherry Leaves")
 
 app.add_page("Cherry Leaves Visualizer", page_cherry_leaves_visualizer_body)
 app.add_page("Powdery Mildew Detector", page_powdery_mildew_detector_body)
+app.add_page("Machine Learning Performance", page_ml_performance_metrics_body)
 
 app.run()
---
+++ app_pages/page_ml_performance.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
+
+
+def page_ml_performance_metrics():
+    version = 'v1'
+
+    st.header("💻 Machine Learning Performance Indicators 💻")
+
+    st.subheader(
+        "📊 Breakdown of Label Frequencies in Train, Validation, and Test Data 📊")
+
+    labels_distribution = plt.imread(
+        f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution,
+             caption='Overview of Label Distribution in Training, Validation, and Test Sets')
+    st.write("---")
+
+    st.subheader("📈 Model History 📈")
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
+
+    st.subheader("📉 Test Data Generalized Outcomes 📉")
+    st.dataframe(pd.DataFrame(load_test_evaluation(
+        version), index=['Loss', 'Accuracy']))
